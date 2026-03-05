4a7222e702f4e57847e1b24c7662925b0eb776f6
kaedracardoso <kaedra.silva17@gmail.com>
2026-03-05 11:38:19 -0300

    organizando o código, adicionando estilos e melhorando a estrutura do projeto.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,14 @@
 function generateNumber() {
 
-    const min = Math.ceil(document.querySelector('.input-min').value)
-    const max = Math.floor(document.querySelector('.input-max').value)
+    const min = Math.ceil(Number(document.querySelector('.input-min').value))
+    const max = Math.floor(Number(document.querySelector('.input-max').value))
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (max > min) {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min
 
-    alert(result);
+        alert(result)
+    } else {
+        alert('O valor mínimo deve ser menor que o valor máximo.')
+    }
+
 }
